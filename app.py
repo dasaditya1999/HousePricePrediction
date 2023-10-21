6dde85dca0dcf9d6172db445bca3ab0110fe9939
Aditya <adityanarayan.das1999@gmail.com>
2023-10-21 13:30:51 +0530

    this commit includes end to end application-v1

--- app.py
+++ app.py
@@ -20,14 +20,22 @@ scaler = pickle.load(open('standardizer.pkl','rb'))
 def home():
     return render_template('home.html')
 
-# Prediction Page
+# Prediction via POSTMAN
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
     scaled_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     prediction  = model.predict(scaled_data)
-    print(prediction[0])
     return jsonify(prediction[0])
+
+# Prediction from the user input
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_data = [float(x) for x in request.form.values()]
+    query = np.array(input_data).reshape(1,-1)
+    final_query = scaler.transform(query)
+    result = model.predict(final_query)
+    return render_template('home.html',prediction_text="the house price is {}".format(result[0]))
 
 # Main function
 if __name__ == "__main__":
